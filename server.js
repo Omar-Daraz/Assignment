cb57453d727978eb0029049ed13bcc8a14ceef62
Omar Daraz <umardaraz22538@gmail.com>
2022-08-21 02:34:00 +0500

    cors fixed

--- server.js
+++ server.js
@@ -22,10 +22,14 @@ console.log("Connected to Database");
 
 
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
-app.use(cors({
-    origin: 'http://localhost:4200/'
-}));
 app.use(bodyParser.json())
 app.get("/",(req,res)=>{
 	res.send("Running")

b9f20af6cfdbd19667d3e2d4975d47a2cc0e1c89
Omar Daraz <umardaraz22538@gmail.com>
2022-08-21 16:40:44 +0500

    cors issue

--- server.js
+++ server.js
@@ -7,29 +7,30 @@ const app = express()
 
 
 const allowedOrigins = [
-    'capacitor://localhost',
-    'ionic://localhost',
-    'http://localhost',
-    'http://localhost:4200',
-    'http://localhost:8100',
-  ];
-  
-  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Origin not allowed by CORS'));
-      }
-    },
-  };
-  
-  // Enable preflight requests for all routes
- 
- app.use(cors(corsOptions))
- app.options('*', cors(corsOptions));
-  
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:4200',
+  'http://localhost:8100',
+  'http://localhost:8080',
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+
+// Enable preflight requests for all routes
+
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions));
+
 const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const cartRoutes = require('./routes/cartRoutes')
@@ -38,18 +39,18 @@ const paymentRoutes = require('./routes/paymentRoutes')
 
 
 mongoose.connect("mongodb+srv://omar:forbidenlove123@cluster0.b5qkeez.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true }).then(res => {
-    {
-        console.log("Db connected")
-    }
+  {
+    console.log("Db connected")
+  }
 }).catch(err => {
-    console.log(err.message)
+  console.log(err.message)
 })
 
 
 
 app.use(bodyParser.json())
 app.get("/", (req, res) => {
-    res.send("Running")
+  res.send("Running")
 })
 
 app.use("/auth", userRoutes)
@@ -58,4 +59,4 @@ app.use("/cart", cartRoutes)
 app.use("/order", orderRoutes)
 app.use("/payment", paymentRoutes)
 
-app.listen(process.env.PORT||5000, () => console.log(`Server is running at ${process.env.PORT}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running at ${process.env.PORT}`));
